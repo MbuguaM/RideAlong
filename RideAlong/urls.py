dcf536ae449aa398263948ffd04f4702c09dffa5
mbugua <munga2mbugua@gmail.com>
2018-05-19 23:50:48 +0300

    first views and templates

--- RideAlong/urls.py
+++ RideAlong/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^cargo/', include('cargo.urls')),
-    url(r'^pilot/', include('pilot.urls'))
+    url(r'^pilot/', include('pilot.urls')),
+    url(r'',TemplateView.as_view(template_name ='homepage.html'), name = 'homepage')
+
 ]
--- cargo/views.py
+++ cargo/views.py
@@ -4,6 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    print(dir(request))
-    print(request.body)
     return HttpResponse('<h1>this is a trial</h1>')
